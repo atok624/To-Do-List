a136ec85d1576a9605743551b87b865e2760ee11
atok624 <84607674+atok624@users.noreply.github.com>
2023-02-23 13:38:35 +0000

    Modify index.js file

--- src/index.js
+++ src/index.js
@@ -1,33 +1,39 @@
-import './style.css';
+import "./style.css";
 
 const toDos = [
   {
-    description: 'Task 1',
+    description: "Task 1",
     completed: false,
     index: 1,
   },
   {
-    description: 'Task 2',
+    description: "Task 2",
     completed: false,
     index: 2,
   },
   {
-    description: 'Task 3',
+    description: "Task 3",
     completed: false,
     index: 3,
   },
 ];
 
-const list = document.querySelector('.todo-list');
-window.addEventListener('DOMContentLoaded', () => {
-  const listString = toDos.map(
-    (task) => `<li class="task">
-<div class="item">
-  <input type="checkbox">
-  <p>${task.description}</p>
-</div>
-<i class="fa-solid fa-ellipsis-vertical"></i> </li>`,
-  )
-    .join('');
-  list.innerHTML = listString;
-});
+const list = document.querySelector(".todo-list");
+
+function todos() {
+  window.addEventListener("DOMContentLoaded", () => {
+    const listString = toDos
+      .map(
+        (task) => `<li class="task">
+  <div class="item">
+    <input type="checkbox">
+    <p>${task.description}</p>
+  </div>
+  <i class="fa-solid fa-ellipsis-vertical"></i> </li>`
+      )
+      .join("");
+    list.innerHTML = listString;
+  });
+}
+
+window.onload = todos;
