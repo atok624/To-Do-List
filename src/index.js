9ffc08294eb81afd0d75f337103cf1527eb45445
atok624 <84607674+atok624@users.noreply.github.com>
2023-02-24 09:06:19 +0000

    Update index.html file

--- src/index.js
+++ src/index.js
@@ -1,39 +1,151 @@
 import './style.css';
+import { saveTasks, loadTasks } from './modules/storage.js';
 
-const toDos = [
-  {
-    description: 'Task 1',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Task 2',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Task 3',
-    completed: false,
-    index: 3,
-  },
-];
+const taskList = document.getElementById('taskList');
+const newTaskForm = document.getElementById('newTaskForm');
+const clearCompletedButton = document.getElementById('clearCompleted');
+let tasks = [];
 
-const list = document.querySelector('.todo-list');
-
-function displayTodos(toDos) {
-  window.addEventListener('DOMContentLoaded', () => {
-    const listString = toDos
-      .map(
-        (task) => `<li class="task">
-  <div class="item">
-    <input type="checkbox">
-    <p>${task.description}</p>
-  </div>
-  <i class="fa-solid fa-ellipsis-vertical"></i> </li>`,
-      )
-      .join('');
-    list.innerHTML = listString;
+const updateIndex = () => {
+  tasks.forEach((element, index) => {
+    element.index = index + 1;
   });
-}
+};
 
-window.onload = displayTodos(toDos);
+const renderTaskList = () => {
+  taskList.innerHTML = '';
+
+  tasks.forEach((task, index) => {
+    const taskElement = document.createElement('li');
+    taskElement.className = 'task';
+    taskElement.draggable = true;
+    taskElement.dataset.index = task.index;
+
+    const taskCheckbox = document.createElement('input');
+    taskCheckbox.type = 'checkbox';
+    taskCheckbox.checked = task.completed;
+    taskCheckbox.addEventListener('change', (event) => {
+      tasks[index].completed = event.target.checked;
+      saveTasks(tasks);
+      renderTaskList();
+    });
+
+    const taskLabel = document.createElement('label');
+    taskLabel.textContent = task.description;
+
+    const taskinput = document.createElement('input');
+    taskinput.type = 'text';
+    taskinput.className = 'disable';
+    taskinput.id = 'editfield';
+
+    const taskDeleteButton = document.createElement('button');
+    taskDeleteButton.type = 'button';
+    taskDeleteButton.innerHTML = '<i class="fa-solid fa-ellipsis-vertical"></i>';
+    taskDeleteButton.addEventListener('click', () => {
+      tasks.splice(index, 1);
+      updateIndex();
+      saveTasks(tasks);
+      renderTaskList();
+    });
+    const taskeditButton = document.createElement('button');
+    taskeditButton.type = 'button';
+    taskeditButton.innerHTML = '<i class="fa fa-pencil"></i>';
+    const tasksaveButton = document.createElement('button');
+    tasksaveButton.type = 'button';
+    tasksaveButton.innerHTML = '<i class="fa fa-floppy-o"></i>';
+    tasksaveButton.className = 'disable';
+
+    taskeditButton.addEventListener('click', () => {
+      tasksaveButton.classList.remove('disable');
+      taskinput.classList.remove('disable');
+      taskinput.value = taskLabel.textContent;
+      taskinput.focus();
+      taskLabel.classList.add('disable');
+      taskeditButton.classList.add('disable');
+    });
+
+    tasksaveButton.addEventListener('click', () => {
+      tasksaveButton.classList.add('disable');
+      taskinput.classList.add('disable');
+      taskLabel.textContent = taskinput.value;
+      tasks[index].description = taskinput.value;
+      taskLabel.classList.remove('disable');
+      taskeditButton.classList.remove('disable');
+      saveTasks(tasks);
+      renderTaskList();
+    });
+
+    taskElement.appendChild(taskCheckbox);
+    taskElement.appendChild(taskLabel);
+    taskElement.appendChild(taskinput);
+    taskElement.appendChild(taskeditButton);
+    taskElement.appendChild(tasksaveButton);
+    taskElement.appendChild(taskDeleteButton);
+
+    taskList.appendChild(taskElement);
+  });
+};
+
+const addTask = (name) => {
+  const task = { index: tasks.length + 1, description: name, completed: false };
+  tasks.push(task);
+  saveTasks(tasks);
+  renderTaskList();
+};
+
+const clearCompletedTasks = () => {
+  tasks = tasks.filter((task) => !task.completed);
+  saveTasks(tasks);
+  renderTaskList();
+};
+
+const handleDragStart = (event) => {
+  event.target.classList.add('dragging');
+};
+
+const handleDragOver = (event) => {
+  event.preventDefault();
+  const taskElement = event.target.closest('.task');
+  const draggingElement = document.querySelector('.dragging');
+  if (taskElement && draggingElement && taskElement !== draggingElement) {
+    const taskElementRect = taskElement.getBoundingClientRect();
+    const dragY = event.clientY - taskElementRect.top;
+    const isAfter = dragY > taskElementRect.height / 2;
+    taskList.insertBefore(
+      draggingElement,
+      isAfter ? taskElement.nextSibling : taskElement,
+    );
+    const newIndex = Array.from(taskList.querySelectorAll('.task')).indexOf(
+      draggingElement,
+    );
+    const oldIndex = draggingElement.getAttribute('data-index');
+    tasks.splice(newIndex, 0, tasks.splice(oldIndex, 1)[0]);
+    saveTasks(tasks);
+  }
+};
+
+const handleDragEnd = (event) => {
+  event.target.classList.remove('dragging');
+};
+
+newTaskForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const newTaskInput = document.getElementById('newTask');
+  const taskName = newTaskInput.value.trim();
+  if (taskName) {
+    addTask(taskName);
+    newTaskInput.value = '';
+    newTaskInput.focus();
+  }
+});
+
+clearCompletedButton.addEventListener('click', () => {
+  clearCompletedTasks();
+});
+
+taskList.addEventListener('dragstart', handleDragStart);
+taskList.addEventListener('dragover', handleDragOver);
+taskList.addEventListener('dragend', handleDragEnd);
+
+tasks = loadTasks();
+renderTaskList();
